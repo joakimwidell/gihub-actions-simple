7ee85f03759a56c5da45a7b609d0a4b0ec01595a
Joakim Widell <70150296+joakimwidell@users.noreply.github.com>
2021-09-08 13:50:26 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gihub_actions_simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
